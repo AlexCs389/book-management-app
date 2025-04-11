61670ad0cda3407c894c0da16b85bf88d3afe94f
Alejandro Cortes <alejandro.cs389@gmail.com>
2025-04-11 12:07:30 -0600

    Se agrego react-router y se creo componente BooksList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import BooksList from '../components/BooksList'
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h1 className="text-center">Aplicación para administrar libros </h1>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          <BooksList />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default Home;
+export default Home
